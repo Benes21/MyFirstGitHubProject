092fcb76c472b46e822cf2f12234b425fe0daff2
Schlick Thobias <Thobias.SCHLICK@htlvb.at>
2021-11-29 08:51:53 +0100

    added Sum-Operation

--- Calculator/Calculator/Program.cs
+++ Calculator/Calculator/Program.cs
@@ -6,7 +6,14 @@ namespace Calculator
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("== Calculator ==");
+            Console.Write("Operand1: ");
+            double operand1 = double.Parse(Console.ReadLine());
+            Console.Write("Operand2: ");
+            double operand2 = double.Parse(Console.ReadLine());
+
+            Console.WriteLine($"Solution = {operand1 / operand2}");
+            Console.WriteLine($"Sum = {operand1 + operand2}");
         }
     }
 }
